336280ee76adb0a9ab14b7508ad4ff00285ff012
zhixian <zhixian@hotmail.com>
2021-11-06 10:18:54 +0800

    Basic src and public files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component} from "react";
+// import "./App.css";
+// import {hot} from "react-hot-loader";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> Hello, World! </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+// export default hot(module)(App);
